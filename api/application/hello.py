e1c45778e8a6c945ba7fe9b3902392b8f743dd76
Jesse <jessealexanderthompson@gmail.com>
2024-12-22 16:34:02 -0500

    Processing async 400k records

--- api/application/hello.py
+++ api/application/hello.py
@@ -3,6 +3,12 @@ from flask_cors import CORS
 import requests
 import json
 import sqlite3
+import pandas as pd
+from multiprocessing.pool import ThreadPool
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__) # Creating a flask instance and putting it into app
 CORS(app)
@@ -55,3 +61,55 @@ def add():
         print("Rows affected", cur.rowcount)
     return data
 
+
+@app.route('/schedule', methods = ['POST'])
+def schedule():
+    BATCH_SIZE = int(os.getenv('BATCH_SIZE'))
+    # With every request, we need the path where the data was put ahead of time
+    data = request.get_json()
+    context_path = data['context_path']
+    print("Context path", context_path)
+    df = pd.read_csv(context_path) # We want to load the data and parse it
+    results = [] # Collect results
+
+    pool = ThreadPool(processes=40)
+    tasks = []
+    if len(df.index) > 0:
+        print("Data Frame", df, "Length", len(df.index))
+        i = 0
+        while i < len(df.index):
+            start = i
+            end = i + BATCH_SIZE
+            i += BATCH_SIZE
+            # process_batch(df, start, end, results) # This is the slow way, synchronous
+            task = pool.apply_async(
+                process_batch,
+                args=(df, start, end)
+            )
+            tasks.append(task)
+    pool.close() # Stop accepting new tasks
+    pool.join() # Wait for all tasks to complete
+
+    print("Batch job complete")
+    for x in tasks:
+        task_output = x.get()
+        results.append(task_output)
+    # Batch the data as its coming in
+    # Run each batch and return into an array of results
+    print("Results", results)
+    return results
+
+# This batch will be x records of total (maybe 10,000)
+def process_batch(df, start, end):
+    # print("Batch", start, end)
+    re = []
+    for i in range(start, end):
+        r = process_record(df.iloc[i, 1], df.iloc[i, 2]) # Map through each and run process_record
+        re.append(r)
+    return re
+
+# Function to apply to single record
+def process_record(col1, col2):
+    # print("process batch", col1, col2)
+    # Run sentiment command from library here
+    return [ col1, col2 ]
